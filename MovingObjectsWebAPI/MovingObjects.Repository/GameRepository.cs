cf030d85862e71e34e836d67c80b361bb8129b8f
stefanpu <stefanpu13@gmail.com>
2014-03-20 02:24:47 +0200

    Added 'Reposity'

---
+++ MovingObjectsWebAPI/MovingObjects.Repository/GameRepository.cs
@@ -0,0 +1,19 @@
+﻿using MovingObjects.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingObjects.Repository
+{
+    public class GameRepository:IRepository<Game>
+    {
+        private MovingObjectsEntities context;
+
+        public IEnumerable<Game> GetAll() 
+        {
+            return context.Games;
+        }
+    }
+}
