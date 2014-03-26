11237545a87482097e6f568ea47bb2f468794a87
stefanpu <stefanpu13@gmail.com>
2014-03-26 09:19:43 +0200

    Added GameControlller and PlayerController

---
+++ MovingObjectsWebAPI/MovingObjects/Controllers/GameController.cs
@@ -0,0 +1,38 @@
+﻿using MovingObjects.Data;
+using MovingObjects.Models;
+using MovingObjects.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MovingObjects.Controllers
+{
+    public class GameController : ApiController
+    {
+        private IRepository<Game> gameRepository;
+
+        public GameController()
+        {
+            this.gameRepository = new GameRepository();
+        }
+
+        // get all games
+
+        public IEnumerable<GameModel> GetGames(int id) 
+        {
+            IEnumerable<GameModel> games = (
+                from game in this.gameRepository.GetAll()
+                select new GameModel 
+                {
+                    Id = game.Id,
+                    Name = game.Name,
+                    SaveDate = game.Date
+                });
+
+            return games;            
+        }
+    }
+}
