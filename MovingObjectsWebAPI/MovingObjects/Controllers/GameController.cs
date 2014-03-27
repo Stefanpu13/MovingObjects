17dbb3e7a49918f4bfdb01fa42db568d983fa7af
stefanpu <stefanpu13@gmail.com>
2014-03-27 18:54:00 +0200

    Added methods to "GameController".
    Added "Get"  "Save" and "Delete" methods.

--- MovingObjectsWebAPI/MovingObjects/Controllers/GameController.cs
+++ MovingObjectsWebAPI/MovingObjects/Controllers/GameController.cs
@@ -12,7 +12,7 @@ namespace MovingObjects.Controllers
 {
     public class GameController : ApiController
     {
-        private IRepository<Game> gameRepository;
+        private IGameRepository<Game> gameRepository;
 
         public GameController()
         {
@@ -20,19 +20,142 @@ namespace MovingObjects.Controllers
         }
 
         // get all games
-
-        public IEnumerable<GameModel> GetGames(int id) 
+        [HttpGet]
+        [ActionName("")]
+        public IEnumerable<GameModel> GetGames(int playerId)
         {
+            var gamesById = 
+                this.gameRepository.GetAll().Where(g => g.PlayerId == playerId);
+
             IEnumerable<GameModel> games = (
-                from game in this.gameRepository.GetAll()
-                select new GameModel 
+                from game in gamesById                 
+                select new GameModel
                 {
                     Id = game.Id,
                     Name = game.Name,
-                    SaveDate = game.Date
-                });
+                    SaveDate = game.Date,
+                    GameObjects = (from gameObject in game.GameObject
+                                   select new GameObjectModel
+                                   {
+                                       Color = gameObject.Color,
+                                       ObjectId = gameObject.ObjectId,
+                                       X = gameObject.X,
+                                       Y = gameObject.Y,
+                                       XDirection = gameObject.XDirection,
+                                       YDirection = gameObject.YDirection
+                                   }).ToList(),
+                    GameState = new GameStateModel 
+                    {
+                        Bonuses = game.GameState.Bonuses,
+                        GameDifficulty = game.GameState.GameDifficulty,
+                        GameLevel = game.GameState.GameLevel,
+                        GameSpeed = game.GameState.GameSpeed,
+                        Restarts = game.GameState.Restarts,
+                        Score = game.GameState.Score
+                    }
+                }
+                );
 
-            return games;            
+            return games;
+        }
+
+        [HttpGet]
+        [ActionName("Load")]
+        public HttpResponseMessage LoadGame(int id)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public HttpResponseMessage SaveGame(int playerId, [FromBody] GameModel gameModel)
+        {
+            HttpResponseMessage response = null;
+            if (gameModel == null)
+            {
+                response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError,
+                    "Can not save empty game");
+                return response;
+            }
+            else
+            {
+                Player player = gameRepository.GetPlayer(playerId);
+                Game game = new Game();
+                if (player != null)
+                {
+                    game.Date = gameModel.SaveDate;
+                    game.Name = gameModel.Name;
+                    game.Player = player;
+                    game.GameState = new GameState
+                    {
+                        Bonuses = gameModel.GameState.Bonuses,
+                        GameDifficulty = gameModel.GameState.GameDifficulty,
+                        GameSpeed = gameModel.GameState.GameSpeed,
+                        GameLevel = gameModel.GameState.GameLevel,
+                        Restarts = gameModel.GameState.Restarts,
+                        Score = gameModel.GameState.Score
+                    };
+                    // TODO: Add game objects.
+                    foreach (var gameObject in gameModel.GameObjects)
+                    {
+                        game.GameObject.Add(new GameObject
+                        {
+                            ObjectId = gameObject.ObjectId,
+                            X = gameObject.X,
+                            Y = gameObject.Y,
+                            XDirection = gameObject.XDirection,
+                            YDirection = gameObject.YDirection,
+                            Color = gameObject.Color
+                        });
+                    }
+
+                    try
+                    {
+                        this.gameRepository.Add(game);
+                    }
+                    catch (Exception ex)
+                    {
+                        response =
+                        Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+
+                        return response;
+                    }
+
+                    response = Request.CreateResponse(HttpStatusCode.OK,
+                        "game added id: " + game.Id);
+
+                }
+                else
+                {
+                    response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError,
+                        "Player with id " + playerId + " was not found.");
+                }
+            }
+
+            return response;
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public HttpResponseMessage DeleteGame(int id) 
+        {
+            HttpResponseMessage response = null;
+            try
+            {
+                var game = this.gameRepository.Delete(id);
+                response = Request.CreateResponse(HttpStatusCode.OK,
+                    "Game with id: " + id + " deleted.");
+            }
+            catch (Exception ex)
+            {
+
+                response = Request.CreateErrorResponse(HttpStatusCode.InternalServerError,
+                    ex);
+            }
+
+            return response;
+            
         }
     }
 }
